8829dc70be0d2846522d7468164f9b0892f7df36
KarinaRedondo <kari24red@gmail.com>
2025-02-14 15:12:38 -0500

    changes dtos registrarse

---
+++ src/main/java/com/tienda/tiendaapp/dtos/RegistrarseDto.java
@@ -0,0 +1,12 @@
+package com.tienda.tiendaapp.dtos;
+
+import com.tienda.tiendaapp.entidades.Role;
+
+public class RegistrarseDto {
+
+    private Role rol;
+    private String nombreCompleto;
+    private String correoElectronico;
+    private String contraseña;
+    private String telefono;
+}
