3622cafbb6e08c4f3f81f40960ae691db903285c
matosr96 <19matos96@gmail.com>
2025-02-19 19:25:52 -0500

    nombre archivo modificado

--- src/main/java/com/tienda/tiendaapp/servicios/Auth/IniciarSesionClienteService.java
+++ src/main/java/com/tienda/tiendaapp/servicios/Auth/IniciarSesionClienteService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class IniciarSesionService {
+public class IniciarSesionClienteService {
 
     @Autowired
     private ClienteRepositorio clienteRepositorio;
