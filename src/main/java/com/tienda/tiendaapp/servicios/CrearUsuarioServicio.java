f86601613a6a8c5c89d4fb1b32ae700b6905d3a6
AlexaPatrici31 <alexapatriciaricardo31@gmail.com>
2025-02-24 16:54:00 -0500

    servicio CrearUsuario

---
+++ src/main/java/com/tienda/tiendaapp/servicios/CrearUsuarioServicio.java
@@ -0,0 +1,29 @@
+package com.tienda.tiendaapp.servicios;
+
+import com.tienda.tiendaapp.dtos.usuario.CrearUsuarioDto;
+import com.tienda.tiendaapp.entidades.Usuario;
+import com.tienda.tiendaapp.repositorios.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class CrearUsuarioServicio {
+    private final UsuarioRepositorio usuarioRepositorio;
+
+    @Autowired
+    public CrearUsuarioServicio (UsuarioRepositorio usuarioRepositorio){
+        this.usuarioRepositorio = usuarioRepositorio;
+    }
+
+    public Usuario crearUsuario(CrearUsuarioDto crearUsuarioDto){
+        Usuario usuario = new Usuario();
+        usuario.setNombreCompleto(crearUsuarioDto.getNombreCompleto());
+        usuario.setCorreo(crearUsuarioDto.getCorreo());
+        usuario.setContraseña(crearUsuarioDto.getContraseña());
+        usuario.setRol(crearUsuarioDto.getRol());
+        usuario.setActivo(crearUsuarioDto.getActivo());
+
+        return usuarioRepositorio.save(usuario);
+    }
+}
